20f181c464f176592a89d631236fb301f93423b4
Shreyansh Jain <sj338191@gmail.com>
2018-05-11 21:38:35 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'app';
+
+  //array for shirts
+  shirts = [
+    {type:"Red Shirt"},
+    {type:"Black Shirt"}
+  ];
+
+  pants = [
+    {type:"Red Pant"},
+    {type:"Black Pant"}
+  ];
+  s1:any={};  
+  s2:any={};
+
+  addShirt(){
+    this.shirts.push(this.s1);
+    this.s1={};
+    alert("Shirt Added to your Wardrobe Successfully");
+  }
+  addPant(){
+    this.pants.push(this.s2);
+    this.s2={};
+    alert("Pant Added to your Wardrobe Successfully");
+
+  }
+
 }
